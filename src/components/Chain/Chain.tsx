d85cf9469bd2a7314687c677a3b2b886f3c30b4e
Rajan Saini <rajansaini691@gmail.com>
2018-09-05 20:37:00 -0700

    Components are now placed dynamically, relative to each other

--- src/components/Chain/Chain.tsx
+++ src/components/Chain/Chain.tsx
@@ -13,22 +13,37 @@ export interface ChainProps {
 }
 
 /**
+ * Stores the different types of objects that can go into the chain for consistency
+ */
+enum chainTypes {"matrix", "vector"}
+
+interface ChainState {
+    // Chain of Strings
+    chain: chainTypes[];
+}
+
+/**
  * Stores the chain of transformations, an animate button
  * to animate the final transformation, and a final result matrix
  * representing what is animated
  */
 export class Chain extends React.Component<ChainProps> {
 
-    data: GraphData
+    data: GraphData;
+    state: ChainState;
 
     constructor(props: ChainProps){
-        super(props)
+        super(props);
 
         this.warpSpace = this.warpSpace.bind(this);
 
         this.data = {
             matrix: [[0, 1], 
-                     [-1, 0]]
+                    [-1, 0]]
+        }
+
+        this.state = {
+            chain: [chainTypes.matrix]
         }
     }
 
@@ -37,11 +52,20 @@ export class Chain extends React.Component<ChainProps> {
     }
 
     render() {
+        // Chain, in this case, is the actual chain of matrices that will be rendered to the DOM
+        let chain: JSX.Element[] = [];
+
+        for(let i = 0; i < this.state.chain.length; i++) {
+            chain.push(<Matrix height = {this.props.height / 2} key = {1} />)
+        }
+
+        console.log("render");
+
         return (
             <div style = {{height: this.props.height, width: this.props.width}} className = {Styles.chain}>
                 <WarpSpace height = {this.props.height / 2} onClick = {this.warpSpace}/>
                 <MenuButton height = {this.props.height / 2}/>
-                <Matrix height = {this.props.height / 2} />
+                {chain}
             </div>
         )
     }
